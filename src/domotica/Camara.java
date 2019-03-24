93c8d4e54c0c322a37ae54a568b81ad4c08ce040
AndreiChirigut <andreichirigut07@gmail.com>
2019-03-24 12:00:58 +0100

    He añadido los metodos de encender y apagar camara

--- src/domotica/Camara.java
+++ src/domotica/Camara.java
@@ -10,7 +10,49 @@ package domotica;
  * @author jairo
  */
 public class Camara {
-        private boolean estado;
-        private String ip;
-    
+
+    private boolean estado;
+    private String ip;
+
+    public Camara(boolean estado, String ip) {
+        this.estado = estado;
+        this.ip = ip;
+    }
+
+    public Camara() {
+
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public void encenderCamara() {
+        if (estado == false) {
+            this.estado = true;
+        } else {
+            System.out.println("Ya esta encendida");
+        }
+    }
+
+    public void apagarCamara() {
+        if (this.estado == true) {
+            this.estado = false;
+        } else {
+            System.out.println("Ya esta apagada");
+        }
+    }
+
 }
