368a0755aab9d136f69def3c5fb2ef574bc3259a
AndreiChirigut <andreichirigut07@gmail.com>
2019-03-24 12:02:05 +0100

    He añadido los metodos de encender y apagar luz

--- src/domotica/Luz.java
+++ src/domotica/Luz.java
@@ -12,5 +12,46 @@ package domotica;
 public class Luz {
     private boolean estado;
     private double consumo;
+
+    public Luz(boolean estado, double consumo) {
+        this.estado = estado;
+        this.consumo = consumo;
+    }
+    
+    public Luz(){
+        
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public double getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(double consumo) {
+        this.consumo = consumo;
+    }
+    
+    public void encenderLuz(){
+        if (estado == false) {
+            this.estado = true;
+        } else {
+            System.out.println("Ya esta encendida");
+        }
+    }
+    
+    public void apagarLuz(){
+        if (estado == true) {
+            this.estado = false;
+        } else {
+            System.out.println("Ya esta apagada");
+        }
+    }
     
 }
