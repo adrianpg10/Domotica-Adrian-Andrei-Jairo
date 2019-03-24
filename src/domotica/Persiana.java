c840bee50a17d7c3c2d5f2e0b00191d91e2f6a2c
AndreiChirigut <andreichirigut07@gmail.com>
2019-03-24 12:02:44 +0100

    He añadido los metodos de subir y bajar persiana

--- src/domotica/Persiana.java
+++ src/domotica/Persiana.java
@@ -13,5 +13,54 @@ public class Persiana {
     private boolean estado;
     private int largo;
     private int ancho;
+
+    public Persiana(boolean estado, int largo, int ancho) {
+        this.estado = estado;
+        this.largo = largo;
+        this.ancho = ancho;
+    }
     
+    public Persiana(){
+        
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+    
+    public void subirPeriana(){
+        if (estado == false) {
+            this.estado = true;
+        } else {
+            System.out.println("Ya estan subidas");
+        }
+    }
+    
+    public void bajarPersiana(){
+        if (estado == true) {
+            this.estado = false;
+        } else {
+            System.out.println("Ya estan bajadas");
+        }
+    }
 }
